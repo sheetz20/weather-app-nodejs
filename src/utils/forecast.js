e1840d84231af0481d75e5b89214e91f7c3ad0bb
sheetal <sheetalshirse20@gmail.com>
2021-05-13 14:47:54 +0530

    first commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const foreCast = (latitude,longitude,callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=13df6f826bd76b83351615eb8a92df8b&query='+ latitude +','+ longitude
+
+    request({url, json: true},(error,{ body }) => {
+        if (error){
+            callback('unable to connect to weather service!', undefined)
+        }
+        else if(body.error){
+            callback('unable to find the location', undefined)
+        }else{
+            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. But it feels like '+body.current.feelslike+' degrees out.')
+        }
+    })
+
+}
+
+
+module.exports = foreCast
---
+++ src/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+const geoCode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2hlZXRhbDk2IiwiYSI6ImNrb2lnZmp1ejAwenoydnF3Y2N4czB3bG4ifQ.AvaBHZJmF2dVxSTylaIKgg'
+    request({url, json: true},(error, { body }) => {
+        if (error){
+            callback('unable to connect to location services!',undefined)
+        }
+        else if (body.features.length === 0){
+            callback('Unable to Find Location. Try another search',undefined)
+        }else{
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+
+
+
+module.exports = geoCode
