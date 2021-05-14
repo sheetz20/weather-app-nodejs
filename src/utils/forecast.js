ac2f4364c7fa01a3f06889418ce127540dcfafd2
sheetal <sheetalshirse20@gmail.com>
2021-05-14 14:56:47 +0530

    changed forecast url

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const foreCast = (latitude,longitude,callback) => {
         else if(body.error){
             callback('unable to find the location', undefined)
         }else{
-            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. But it feels like '+body.current.feelslike+' degrees out.')
+            callback(undefined,body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. But it feels like '+body.current.feelslike+' degrees out. And the humidity is '+body.current.humidity+' percent.')
         }
     })
 
